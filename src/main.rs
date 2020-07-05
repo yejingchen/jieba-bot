ac9d035ea5df0ebd3ec5177c9db9825ef6638be8
Ye Jingchen <ye.jingchen@gmail.com>
2020-07-06 00:25:52 +0800

    add https proxy support, runtime bot token, HMM
    HMM = Hidden Markov Models in the Jieba algorithm.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use hyper_proxy::{Proxy, Intercept};
 use lazy_static::lazy_static;
 use jieba_rs::Jieba;
 use tbot::types::{
@@ -6,18 +7,17 @@ use tbot::types::{
     parameters::Text as ParseMode,
 };
 
+const BOT_TOKEN_ENV: &str = "BOT_TOKEN";
+
 lazy_static! {
     static ref JIEBA: Jieba = Jieba::new();
 }
 
-fn make_inline_result<'a>(
-    id: &'a inline_query::Id,
-    title: &'a str,
-    msg: &'a str
-) -> inline_query::Result<'a> {
+fn make_inline_result<'a>(id: &'a str, title: &'a str, msg: &'a str)
+-> inline_query::Result<'a> {
     let content = Text::new(ParseMode::plain(msg));
     let article = Article::new(title, content).description(msg);
-    let result = inline_query::Result::new(&id.0, article);
+    let result = inline_query::Result::new(id, article);
     return result;
 }
 
@@ -26,21 +26,39 @@ async fn main() {
     env_logger::init();
     log::info!("jieba-bot: Chinese word segmentation Telegram bot");
 
-    // BOT_TOKEN is a compile time environment variable
-    let mut bot = tbot::from_env!("BOT_TOKEN").event_loop();
+    let mut bot = match std::env::var("HTTPS_PROXY") {
+        Ok(proxy_uri) => {
+            log::info!("Using https proxy {}", proxy_uri);
+            let proxy_uri = proxy_uri.parse().unwrap();
+            let proxy = Proxy::new(Intercept::Https, proxy_uri);
+
+            tbot::Bot::from_env_with_proxy(BOT_TOKEN_ENV, proxy).event_loop()
+        },
+        Err(e) => {
+            log::info!("Not using proxy because {}", e);
+
+            tbot::Bot::from_env(BOT_TOKEN_ENV).event_loop()
+        }
+    };
 
     bot.inline(|ctx| {
         async move {
-            let (title, msg) = if ctx.query.is_empty() {
-                ("输入点文字来分词", "就像这样")
+            if ctx.query.is_empty() {
+                let result = make_inline_result(&ctx.id.0, "输入点文字来分词", "就 会 像 这样");
+                if let Err(e) = ctx.answer(&[result]).call().await {
+                    log::warn!("Default message (id={}) failed with: {}", &ctx.id.0, e);
+                }
             } else {
-                ("分词大成功！", &ctx.query[..])
-            };
-            let answer = JIEBA.cut(msg, false).join(" ");
+                let answer = JIEBA.cut(&ctx.query, false).join(" ");
+                let result = make_inline_result(&ctx.id.0, "分词大成功！", &answer);
 
-            let result = make_inline_result(&ctx.id, title, &answer);
-            if let Err(e) =  ctx.answer(&[result]).call().await {
-                log::warn!("answer InlineQuery failed with: {:?}", e);
+                let hmm_answer = JIEBA.cut(&ctx.query, true).join(" ");
+                let hmm_id = format!("{}_hmm", ctx.id.0);
+                let hmm_result = make_inline_result(&hmm_id, "马尔可夫 ON！", &hmm_answer);
+
+                if let Err(e) = ctx.answer(&[result, hmm_result]).call().await {
+                    log::warn!("Answering InlineQuery (id={}) failed with: {}", &ctx.id.0, e);
+                }
             }
         }
     });
